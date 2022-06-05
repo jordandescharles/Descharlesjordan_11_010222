231805e370b31cebf7741e17ab853551ad5576ba
jordandescharles <73353820+jordandescharles@users.noreply.github.com>
2022-06-05 12:05:43 +0200

    update deploy

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './css/style.css'
 
 const rootElement = document.getElementById("root");
 render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
       <Route path="/" element={<App />} />
       <Route path="About" element={<About />} />
